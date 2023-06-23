ddad203905685ecc1ced40f2466cc34e0cf27022
João <joao@Pro-de-bionet.alunos.fc.ul.pt>
2023-06-23 18:09:06 +0100

    Updated R scripts.

--- R/oplsda_string.R
+++ R/oplsda_string.R
@@ -1,5 +1,9 @@
+
+BiocManager::install("ropls")
+
 library(foreach)
 library(doParallel)
+
 
 ####CUSTOM FUNCTIONS####
 
@@ -14,7 +18,7 @@ oplsda.fs <- function(metrics, labels){
     train.index <- unlist(createDataPartition(column_labels, p = .8, list = TRUE))
     all_labels  <- seq(1, nrow(labels))
     test.index <- setdiff(all_labels, train.index)
-    if(length(test.index) == 3806){
+    if(length(test.index) == 3275){
       test.index <- append(test.index, NA)
     }
     process.oplsda <- try(opls(metrics, column_labels, orthoI = NA, predI=1, crossvalI = 10, subset=train.index, fig.pdfC = "none", info.txtC='none'), silent=TRUE)
@@ -34,129 +38,135 @@ oplsda.fs <- function(metrics, labels){
 ####DATA LOAD####
 
 #####PROCESS#####
-
+####-------------Normal modules---------------------------------------####
 reactome_labels <- read.csv('../python/data/processed/reactome_labels_string.csv', header=FALSE)
 rwr <- read.csv('../python/data/processed/metrics/process_rwr_string.csv', row.names = 1)
-genePANDA <- read.csv('../python/data/processed/metrics/process_genePANDA_string.csv', row.names = 1)
-maxlink <- read.csv('../python/data/processed/metrics/process_maxlink_string.csv', row.names = 1)
-raw <- read.csv('../python/data/processed/metrics/process_raw_string.csv', row.names = 1)
+
+#genePANDA <- read.csv('../python/data/processed/metrics/process_genePANDA_string.csv', row.names = 1)
+#maxlink <- read.csv('../python/data/processed/metrics/process_maxlink_string.csv', row.names = 1)
+#raw <- read.csv('../python/data/processed/metrics/process_raw_string.csv', row.names = 1)
 
 row.names(reactome_labels) <- row.names(rwr)
 colnames(reactome_labels) <- colnames(rwr)
 
+####-------------False annotation modules---------------------------------------####
 reactome_labels_fp <- read.csv('../python/data/processed/string_reactome_labels_fp.csv', header=FALSE)
 rwr_fp <- read.csv('../python/data/processed/metrics/string_process_rwr_fp.csv', row.names = 1)
-raw_fp <- read.csv('../python/data/processed/metrics/process_raw_fp_string.csv', row.names = 1)
+
+#raw_fp <- read.csv('../python/data/processed/metrics/process_raw_fp_string.csv', row.names = 1)
+
 row.names(reactome_labels_fp) <- row.names(rwr_fp)
 colnames(reactome_labels_fp) <- colnames(rwr_fp)
 
 #####DISEASE#####
 ######SCA######
 
+####-------------Normal modules---------------------------------------####
 disgenet_labels <- read.csv('../python/data/processed/disgenet_sca_labels_string.csv', header=FALSE)
 disease_rwr <- read.csv('../python/data/processed/metrics/disease_rwr_string.csv', row.names = 1)
-disease_genePANDA <- read.csv('../python/data/processed/metrics/disease_genePANDA_string.csv', row.names = 1)
-disease_maxlink <- read.csv('../python/data/processed/metrics/disease_maxlink_string.csv', row.names = 1)
-disease_raw <- read.csv('../python/data/processed/metrics/disease_raw_string.csv', row.names = 1)
+
+#disease_genePANDA <- read.csv('../python/data/processed/metrics/disease_genePANDA_string.csv', row.names = 1)
+#disease_maxlink <- read.csv('../python/data/processed/metrics/disease_maxlink_string.csv', row.names = 1)
+#disease_raw <- read.csv('../python/data/processed/metrics/disease_raw_string.csv', row.names = 1)
 
 row.names(disgenet_labels) <- row.names(disease_rwr)
 colnames(disgenet_labels) <- colnames(disease_rwr)
 
+####-------------False annotation modules---------------------------------------####
 disgenet_labels_fp <- read.csv('../python/data/processed/string_disgenet_labels_fp.csv', header=FALSE)
 disease_rwr_fp <- read.csv('../python/data/processed/metrics/string_disease_rwr_fp.csv', row.names = 1)
-disease_raw_fp <- read.csv('../python/data/processed/metrics/disease_raw_fp_string.csv', row.names = 1)
+
+#disease_raw_fp <- read.csv('../python/data/processed/metrics/disease_raw_fp_string.csv', row.names = 1)
+
 row.names(disgenet_labels_fp) <- row.names(disease_rwr_fp)
 colnames(disgenet_labels_fp) <- colnames(disease_rwr_fp)
 
 ######CONSERVATIVE######
 
+####-------------Normal modules---------------------------------------####
 disgenet_labels_conservative <- read.csv('../python/data/processed/disgenet_conservative_labels_string.csv', header=FALSE)
 disease_rwr_conservative <- read.csv('../python/data/processed/metrics/disease_rwr_conservative_string.csv', row.names = 1)
-disease_genePANDA_conservative <- read.csv('../python/data/processed/metrics/disease_conservative_genePANDA_string.csv', row.names = 1)
-disease_maxlink_conservative <- read.csv('../python/data/processed/metrics/disease_conservative_maxlink_string.csv', row.names = 1)
-disease_raw_conservative <- read.csv('../python/data/processed/metrics/disease_conservative_raw_string.csv', row.names = 1)
+
+#disease_genePANDA_conservative <- read.csv('../python/data/processed/metrics/disease_conservative_genePANDA_string.csv', row.names = 1)
+#disease_maxlink_conservative <- read.csv('../python/data/processed/metrics/disease_conservative_maxlink_string.csv', row.names = 1)
+#disease_raw_conservative <- read.csv('../python/data/processed/metrics/disease_conservative_raw_string.csv', row.names = 1)
 
 row.names(disgenet_labels_conservative) <- row.names(disease_rwr_conservative)
 colnames(disgenet_labels_conservative) <- colnames(disease_rwr_conservative)
 
+####-------------False annotation modules---------------------------------------####
 disgenet_labels_conservative_fp <- read.csv('../python/data/processed/string_disgenet_conservative_labels_fp.csv', header=FALSE)
 disease_rwr_conservative_fp <- read.csv('../python/data/processed/metrics/string_disease_rwr_conservative_fp.csv', row.names = 1)
-disease_raw_conservative_fp <- read.csv('../python/data/processed/metrics/disease_raw_fp_string.csv', row.names = 1)
+
+#disease_raw_conservative_fp <- read.csv('../python/data/processed/metrics/disease_raw_fp_string.csv', row.names = 1)
+
 row.names(disgenet_labels_conservative_fp) <- row.names(disease_rwr_conservative_fp)
 colnames(disgenet_labels_conservative_fp) <- colnames(disease_rwr_conservative_fp)
 
+
 ####FEATURE SELECTION PROCESS####
 
+####-------------Normal modules---------------------------------------####
 rwr_fs <- oplsda.fs(rwr, reactome_labels)
 write.csv(as.data.frame(rwr_fs[,1]),"../python/data/processed/fs/reactome_rwr_fs_string.csv", row.names = FALSE)
 write.csv(as.data.frame(rwr_fs[,2]),"../python/data/processed/fs/reactome_rwr_test_string.csv", row.names = FALSE)
 
-genePANDA_fs <- oplsda.fs(genePANDA, reactome_labels)
-write.csv(as.data.frame(genePANDA_fs[,1]),"../python/data/processed/fs/reactome_genePANDA_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(genePANDA_fs[,2]),"../python/data/processed/fs/reactome_genePANDA_test_string.csv", row.names = FALSE)
-
-maxlink_fs <- oplsda.fs(maxlink, reactome_labels)
-write.csv(as.data.frame(maxlink_fs[,1]),"../python/data/processed/fs/reactome_maxlink_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(maxlink_fs[,2]),"../python/data/processed/fs/reactome_maxlink_test_string.csv", row.names = FALSE)
-
-raw_fs <- oplsda.fs(raw, reactome_labels)
-write.csv(as.data.frame(raw_fs[,1]),"../python/data/processed/fs/reactome_raw_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(raw_fs[,2]),"../python/data/processed/fs/reactome_raw_test_string.csv", row.names = FALSE)
-
-
+####-------------False annotation modules---------------------------------------####
 rwr_fp_fs <- oplsda.fs(rwr_fp, reactome_labels_fp)
 write.csv(as.data.frame(rwr_fp_fs[,1]),"../python/data/processed/fs/reactome_rwr_fp_fs_string.csv", row.names = FALSE)
 write.csv(as.data.frame(rwr_fp_fs[,2]),"../python/data/processed/fs/reactome_rwr_fp_test_string.csv", row.names = FALSE)
-raw_fp_fs <- oplsda.fs(raw_fp, reactome_labels_fp)
-write.csv(as.data.frame(raw_fp_fs[,1]),"../python/data/processed/fs/reactome_raw_fp_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(raw_fp_fs[,2]),"../python/data/processed/fs/reactome_raw_fp_test_string.csv", row.names = FALSE)
+
 
 ####FEATURE SELECTION DISEASE####
 #####SCA#####
 
+####-------------Normal modules---------------------------------------####
 disease_rwr_fs <- oplsda.fs(disease_rwr, disgenet_labels)
 write.csv(as.data.frame(disease_rwr_fs[,1]),"../python/data/processed/fs/disease/disease_rwr_fs_string.csv", row.names = FALSE)
 write.csv(as.data.frame(disease_rwr_fs[,2]),"../python/data/processed/fs/disease/disease_rwr_test_string.csv", row.names = FALSE)
 
-disease_genePANDA_fs <- oplsda.fs(disease_genePANDA, disgenet_labels)
-write.csv(as.data.frame(disease_genePANDA_fs[,1]),"../python/data/processed/fs/disease/disease_genePANDA_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(disease_genePANDA_fs[,2]),"../python/data/processed/fs/disease/disease_genePANDA_test_string.csv", row.names = FALSE)
-
-disease_maxlink_fs <- oplsda.fs(disease_maxlink, disgenet_labels)
-write.csv(as.data.frame(disease_maxlink_fs[,1]),"../python/data/processed/fs/disease/disease_maxlink_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(disease_maxlink_fs[,2]),"../python/data/processed/fs/disease/disease_maxlink_test_string.csv", row.names = FALSE)
-
-disease_raw_fs <- oplsda.fs(disease_raw, disgenet_labels)
-write.csv(as.data.frame(disease_raw_fs[,1]),"../python/data/processed/fs/disease/disease_raw_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(disease_raw_fs[,2]),"../python/data/processed/fs/disease/disease_raw_test_string.csv", row.names = FALSE)
-
-
+####-------------False annotation modules---------------------------------------####
 disease_rwr_fp_fs <- oplsda.fs(disease_rwr_fp, disgenet_labels_fp)
 write.csv(as.data.frame(disease_rwr_fp_fs[,1]),"../python/data/processed/fs/disease/disease_rwr_fp_fs_string.csv", row.names = FALSE)
 write.csv(as.data.frame(disease_rwr_fp_fs[,2]),"../python/data/processed/fs/disease/disease_rwr_fp_test_string.csv", row.names = FALSE)
-disease_raw_fp_fs <- oplsda.fs(disease_raw_fp, disgenet_labels_fp)
-write.csv(as.data.frame(disease_raw_fp_fs[,1]),"../python/data/processed/fs/disease/disease_raw_fp_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(disease_raw_fp_fs[,2]),"../python/data/processed/fs/disease/disease_raw_fp_test_string.csv", row.names = FALSE)
+
 
 #####CONSERVATIVE MODULE#####
 
+####-------------Normal modules---------------------------------------####
 disease_rwr_fs_conservative <- oplsda.fs(disease_rwr_conservative, disgenet_labels_conservative)
 write.csv(as.data.frame(disease_rwr_fs_conservative[,1]),"../python/data/processed/fs/disease/disease_rwr_fs_conservative_string.csv", row.names = FALSE)
 write.csv(as.data.frame(disease_rwr_fs_conservative[,2]),"../python/data/processed/fs/disease/disease_rwr_test_conservative_string.csv", row.names = FALSE)
 
-disease_genePANDA_fs_conservative <- oplsda.fs(disease_genePANDA_conservative, disgenet_labels_conservative)
-write.csv(as.data.frame(disease_genePANDA_fs_conservative[,1]),"../python/data/processed/fs/disease/disease_genePANDA_fs_conservative_string.csv", row.names = FALSE)
-write.csv(as.data.frame(disease_genePANDA_fs_conservative[,2]),"../python/data/processed/fs/disease/disease_genePANDA_test_conservative_string.csv", row.names = FALSE)
-
-disease_raw_fs_conservative <- oplsda.fs(disease_raw_conservative, disgenet_labels_conservative)
-write.csv(as.data.frame(disease_raw_fs_conservative[,1]),"../python/data/processed/fs/disease/disease_raw_fs_conservative_string.csv", row.names = FALSE)
-write.csv(as.data.frame(disease_raw_fs_conservative[,2]),"../python/data/processed/fs/disease/disease_raw_test_conservative_string.csv", row.names = FALSE)
-
-
+####-------------False annotation modules---------------------------------------####
 disease_rwr_fp_fs_conservative <- oplsda.fs(disease_rwr_conservative_fp, disgenet_labels_conservative_fp)
 write.csv(as.data.frame(disease_rwr_fp_fs_conservative[,1]),"../python/data/processed/fs/disease/disease_rwr_fp_fs_conservative_string.csv", row.names = FALSE)
 write.csv(as.data.frame(disease_rwr_fp_fs_conservative[,2]),"../python/data/processed/fs/disease/disease_rwr_fp_test_conservative_string.csv", row.names = FALSE)
-disease_raw_fp_fs_conservative <- oplsda.fs(disease_raw_conservative_fp, disgenet_labels_conservative_fp)
-write.csv(as.data.frame(disease_raw_fp_fs_conservative[,1]),"../python/data/processed/fs/disease/disease_raw_fp_fs_conservative_string.csv", row.names = FALSE)
-write.csv(as.data.frame(disease_raw_fp_fs_conservative[,2]),"../python/data/processed/fs/disease/disease_raw_fp_test_conservative_string.csv", row.names = FALSE)
 
+
+#####Raw####
+#raw_fs <- oplsda.fs(raw, reactome_labels)
+#write.csv(as.data.frame(raw_fs[,1]),"../python/data/processed/fs/reactome_raw_fs_string.csv", row.names = FALSE)
+#write.csv(as.data.frame(raw_fs[,2]),"../python/data/processed/fs/reactome_raw_test_string.csv", row.names = FALSE)
+
+#raw_fp_fs <- oplsda.fs(raw_fp, reactome_labels_fp)
+#write.csv(as.data.frame(raw_fp_fs[,1]),"../python/data/processed/fs/reactome_raw_fp_fs_string.csv", row.names = FALSE)
+#write.csv(as.data.frame(raw_fp_fs[,2]),"../python/data/processed/fs/reactome_raw_fp_test_string.csv", row.names = FALSE)
+
+
+#disease_raw_fs <- oplsda.fs(disease_raw, disgenet_labels)
+#write.csv(as.data.frame(disease_raw_fs[,1]),"../python/data/processed/fs/disease/disease_raw_fs_string.csv", row.names = FALSE)
+#write.csv(as.data.frame(disease_raw_fs[,2]),"../python/data/processed/fs/disease/disease_raw_test_string.csv", row.names = FALSE)
+
+#disease_raw_fp_fs <- oplsda.fs(disease_raw_fp, disgenet_labels_fp)
+#write.csv(as.data.frame(disease_raw_fp_fs[,1]),"../python/data/processed/fs/disease/disease_raw_fp_fs_string.csv", row.names = FALSE)
+#write.csv(as.data.frame(disease_raw_fp_fs[,2]),"../python/data/processed/fs/disease/disease_raw_fp_test_string.csv", row.names = FALSE)
+
+
+#disease_raw_fs_conservative <- oplsda.fs(disease_raw_conservative, disgenet_labels_conservative)
+#write.csv(as.data.frame(disease_raw_fs_conservative[,1]),"../python/data/processed/fs/disease/disease_raw_fs_conservative_string.csv", row.names = FALSE)
+#write.csv(as.data.frame(disease_raw_fs_conservative[,2]),"../python/data/processed/fs/disease/disease_raw_test_conservative_string.csv", row.names = FALSE)
+
+#disease_raw_fp_fs_conservative <- oplsda.fs(disease_raw_conservative_fp, disgenet_labels_conservative_fp)
+#write.csv(as.data.frame(disease_raw_fp_fs_conservative[,1]),"../python/data/processed/fs/disease/disease_raw_fp_fs_conservative_string.csv", row.names = FALSE)
+#write.csv(as.data.frame(disease_raw_fp_fs_conservative[,2]),"../python/data/processed/fs/disease/disease_raw_fp_test_conservative_string.csv", row.names = FALSE)
--- R/raw_string.R
+++ R/raw_string.R
@@ -1,22 +1,38 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("diffuStats")
+
+
 library(igraph)
 library(diffuStats)
 library(foreach)
 library(doParallel)
 
+#-------------Normal modules---------------------------------------#
 process_modules <- read.csv('../python/data/processed/string_reactome_modules.csv')
-disease_modules <- read.csv('../python/data/processed/string_disgenet_modules.csv')
-process_modules_fp <- read.csv('../python/data/processed/string_reactome_modules_fp.csv')
-disease_modules_fp <- read.csv('../python/data/processed/string_disgenet_sca_modules_fp.csv')
-disease_conservative_modules_fp <- read.csv('../python/data/processed/string_disgenet_conservative_modules_fp.csv')
-ppi_graph <- read_graph("../python/data/processed/graph_string", format='gml')
+disease_conservative_modules <- read.csv('../python/data/processed/string_disgenet_modules.csv')
+disease_modules <- disease_conservative_modules[disease_conservative_modules$increase<0.4,]
 
 reactome_labels <- read.csv('../python/data/processed/reactome_labels_string.csv', header=FALSE)
 disgenet_labels <- read.csv('../python/data/processed/disgenet_sca_labels_string.csv', header=FALSE)
 disgenet_conservative_labels <- read.csv('../python/data/processed/disgenet_conservative_labels_string.csv', header=FALSE)
+
+
+#-------------False annotation modules---------------------------------------#
+process_modules_fp <- read.csv('../python/data/processed/string_reactome_modules_fp.csv')
+disease_modules_fp <- read.csv('../python/data/processed/string_disgenet_sca_modules_fp.csv')
+disease_conservative_modules_fp <- read.csv('../python/data/processed/string_disgenet_conservative_modules_fp.csv')
+
 reactome_labels_fp <- read.csv('../python/data/processed/string_reactome_labels_fp.csv', header=FALSE)
 disgenet_labels_fp <- read.csv('../python/data/processed/string_disgenet_labels_fp.csv', header=FALSE)
 disgenet_conservative_labels_fp <- read.csv('../python/data/processed/string_disgenet_conservative_labels_fp.csv', header=FALSE)
-colnames(reactome_labels) <- process_modules[['process']]
+
+#-------------PPI graph---------------------------------------#
+ppi_graph <- read_graph("../python/data/processed/graph_string", format='gml')
+
+
+#colnames(reactome_labels) <- process_modules[['process']]
 rownames(reactome_labels) <- V(ppi_graph)$name
 rownames(disgenet_labels) <- V(ppi_graph)$name
 rownames(disgenet_conservative_labels) <- V(ppi_graph)$name
@@ -29,7 +45,9 @@ kernel_scores <- regularisedLaplacianKernel(ppi_graph)
 
 ####PROCESS####
 
-process_raw_scores <- matrix(0,length(process_modules[['process']]) , length(V(ppi_graph)$name))
+#------------------Normal modules--------------------------------------
+#
+process_raw_scores <- matrix(0, length(process_modules[['process']]), length(V(ppi_graph)$name))
 rownames(process_raw_scores) <- process_modules[['process']]
 colnames(process_raw_scores) <- V(ppi_graph)$name
 
@@ -50,11 +68,11 @@ write.csv(t(process_raw_scores),"../python/data/processed/metrics/process_raw_st
 
 process_raw_scores <- read.csv('../python/data/processed/metrics/process_raw_string.csv', row.names = 1)
 
-process_raw_fs <- oplsda.fs(process_raw_scores, reactome_labels)
-write.csv(as.data.frame(process_raw_fs[,1]),"../python/data/processed/fs/process_raw_fs_string.csv", row.names = FALSE)
-write.csv(as.data.frame(process_raw_fs[,2]),"../python/data/processed/fs/process_raw_test_string.csv", row.names = FALSE)
+#process_raw_fs <- oplsda.fs(process_raw_scores, reactome_labels)
+#write.csv(as.data.frame(process_raw_fs[,1]),"../python/data/processed/fs/process_raw_fs_string.csv", row.names = FALSE)
+#write.csv(as.data.frame(process_raw_fs[,2]),"../python/data/processed/fs/process_raw_test_string.csv", row.names = FALSE)
 
-
+#------------------False annotation modules--------------------------------------
 process_raw_scores_fp <- matrix(0,length(process_modules_fp[['process']]) , length(V(ppi_graph)$name))
 rownames(process_raw_scores_fp) <- process_modules_fp[['process']]
 colnames(process_raw_scores_fp) <- V(ppi_graph)$name
@@ -74,8 +92,30 @@ for(i in 1:length(process_modules_fp[['process']])){
 }
 write.csv(t(process_raw_scores_fp),"../python/data/processed/metrics/process_raw_fp_string.csv")
 
+
 ####DISEASE SCA####
 
+#------------------Normal modules--------------------------------------
+disease_raw_scores <- matrix(0, length(disease_modules[['process']]), length(V(ppi_graph)$name))
+rownames(disease_raw_scores) <- disease_modules[['process']]
+colnames(disease_raw_scores) <- V(ppi_graph)$name
+
+for(i in 1:length(disease_modules[['process']])){
+  module_scores <- disgenet_labels[,i]
+  names(module_scores) <- rownames(disgenet_labels)
+  seleted_scores <- disgenet_labels[,i]
+  names(seleted_scores) <- rownames(disgenet_labels)
+  df_diff <- diffuse_grid(graph=ppi_graph,
+                          K = kernel_scores,
+                          scores = seleted_scores,
+                          grid_param = expand.grid(method = c('raw')),
+                          n.perm = 1000)
+  disease_raw_scores[i,] = df_diff$node_score
+}
+write.csv(t(disease_raw_scores),"../python/data/processed/metrics/disease_raw_string.csv")
+
+
+#------------------False annotation modules--------------------------------------
 disease_raw_scores_fp <- matrix(0,length(disease_modules_fp[['process']]) , length(V(ppi_graph)$name))
 rownames(disease_raw_scores_fp) <- disease_modules_fp[['process']]
 colnames(disease_raw_scores_fp) <- V(ppi_graph)$name
@@ -97,6 +137,28 @@ write.csv(t(disease_raw_scores_fp),"../python/data/processed/metrics/disease_raw
 
 ####DISEASE CONSERVATIVE####
 
+#------------------Normal modules--------------------------------------
+disease_conservative_raw_scores <- matrix(0,length(disease_conservative_modules[['process']]) , length(V(ppi_graph)$name))
+rownames(disease_conservative_raw_scores) <- disease_conservative_modules[['process']]
+colnames(disease_conservative_raw_scores) <- V(ppi_graph)$name
+
+
+for(i in 1:length(disease_conservative_modules[['process']])){
+  module_scores <- disgenet_conservative_labels[,i]
+  names(module_scores) <- rownames(disgenet_conservative_labels)
+  seleted_scores <- disgenet_conservative_labels[,i]
+  names(seleted_scores) <- rownames(disgenet_conservative_labels)
+  df_diff <- diffuse_grid(graph=ppi_graph,
+                          K = kernel_scores,
+                          scores = seleted_scores,
+                          grid_param = expand.grid(method = c('raw')),
+                          n.perm = 1000)
+  disease_conservative_raw_scores[i,] = df_diff$node_score
+}
+write.csv(t(disease_conservative_raw_scores),"../python/data/processed/metrics/disease_conservative_raw_string.csv")
+
+
+#------------------False annotation modules--------------------------------------
 disease_conservative_raw_scores_fp <- matrix(0,length(disease_conservative_modules_fp[['process']]) , length(V(ppi_graph)$name))
 rownames(disease_conservative_raw_scores_fp) <- disease_conservative_modules_fp[['process']]
 colnames(disease_conservative_raw_scores_fp) <- V(ppi_graph)$name
